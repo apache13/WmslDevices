7b7dddb0a17b3e2f89f19992566fefe652365b46
apache13 <apache13@gmail.com>
2017-07-16 22:57:55 +0700

    Improve feature print tag

--- app/controllers/tag_controller.rb
+++
@@ -1,5 +0,0 @@
-class TagController < ApplicationController
-  def print
-    @device = Device.find(params[:id])
-  end
-end
--- test/controllers/tag_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TagControllerTest < ActionDispatch::IntegrationTest
-  test "should get print" do
-    get tag_print_url
-    assert_response :success
-  end
-
-end
